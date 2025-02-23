9c3d5228f93187b448c67f55e42d45a1966f877e
scg984261 <scottgillen@hotmail.com>
2025-02-23 12:04:50 +0000

    Setting the gridcolumn property value of an element on the UI

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonMoveNavigation_Click(object sender, RoutedEventArgs eventArgs)
+        {
+            int column = (int) this.customerListGrid.GetValue(Grid.ColumnProperty);
+
+            int newColumn = column == 0 ? 2 : 0;
+
+            this.customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+        }
     }
-  }
 }
