332e6bddcd816b5961bd58c7d5bfb67fc750cb2e
scg984261 <scottgillen@hotmail.com>
2025-02-23 16:16:57 +0000

    Using viewmodel for customer data

--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
@@ -8,9 +10,19 @@ namespace WiredBrainCoffee.CustomersApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel customerViewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+            this.customerViewModel = new CustomersViewModel(new CustomerDataProvider());
+            this.DataContext = this.customerViewModel;
+            this.Loaded += this.CustomersView_Loaded;
+        }
+
+        public async void CustomersView_Loaded(object sender, RoutedEventArgs eventArgs)
+        {
+            await customerViewModel.LoadAsync();
         }
 
         public void ButtonMoveNavigation_Click(object sender, RoutedEventArgs eventArgs)
---
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Linq;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomersViewModel
+    {
+        private readonly ICustomerDataProvider customerDataProvider;
+        public ObservableCollection<Customer> Customers { get; } = new ObservableCollection<Customer>();
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            this.customerDataProvider = customerDataProvider;
+        }
+
+        public async Task LoadAsync()
+        {
+            if (this.Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await this.customerDataProvider.GetAllAsync();
+
+            if (customers != null)
+            {
+                foreach (Customer customer in customers)
+                {
+                    this.Customers.Add(customer);
+                }
+            }
+        }
+    }
+}
