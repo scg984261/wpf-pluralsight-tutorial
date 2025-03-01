5b4dd20b3c8977617363a5f963e41abb8360f860
scg984261 <scottgillen@hotmail.com>
2025-03-01 19:14:42 +0000

    Adding dependency injection for productdataprovider

--- WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using WiredBrainCoffee.CustomersApp.Data;
 using WiredBrainCoffee.CustomersApp.ViewModel;
 
@@ -6,11 +7,30 @@ namespace WiredBrainCoffee.CustomersApp
 {
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            this.ConfigureServices(services);
+            this.serviceProvider = services.BuildServiceProvider();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            MainWindow mainWindow = new MainWindow(new MainViewModel(new CustomersViewModel(new CustomerDataProvider()), new ProductsViewModel()));
-            mainWindow.Show();
+            MainWindow? mainWindow = this.serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomersViewModel>();
+            services.AddTransient<ProductsViewModel>();
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+            services.AddTransient<IProductDataProvider, ProductDataProvider>();
         }
     }
 }
--- WiredBrainCoffee.CustomersApp/ViewModel/ProductsViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/ProductsViewModel.cs
@@ -1,7 +1,38 @@
-﻿namespace WiredBrainCoffee.CustomersApp.ViewModel
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
     public class ProductsViewModel : ViewModelBase
     {
+        private readonly IProductDataProvider productDataProvider;
+        public ObservableCollection<Product> Products { get; }
 
+        public ProductsViewModel(IProductDataProvider productDataProvider)
+        {
+            this.productDataProvider = productDataProvider;
+            this.Products = new ObservableCollection<Product>();
+        }
+
+        public override async Task LoadAsync()
+        {
+            if (this.Products.Any())
+            {
+                return;
+            }
+
+            var products = await productDataProvider.GetAllAsync();
+
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    this.Products.Add(product);
+                }
+            }
+        }
     }
 }
